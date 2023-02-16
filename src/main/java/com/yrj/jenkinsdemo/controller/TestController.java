9c77fd7c4fdc986f5b89840cd14bae9b0e1b750a
Wade <735345044@qq.com>
2023-02-16 21:19:53 +0800

    first

---
+++ src/main/java/com/yrj/jenkinsdemo/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.yrj.jenkinsdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/test")
+    public String test () {
+        return "test";
+    }
+}
